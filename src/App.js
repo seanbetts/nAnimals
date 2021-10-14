4204f47f40de9b0a129e7206e2d1d6170a1bffe3
seanbetts <seanbetts@icloud.com>
2021-10-14 13:57:51 +0100

    Simplified for COMING SOON!

--- src/App.js
+++ src/App.js
@@ -1,73 +1,11 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { connect } from "./redux/blockchain/blockchainActions";
-import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import nEggLogo from "./images/nEggLogoTitle.png";
 import discord from "./images/discord.png";
 import twitter from "./images/twitter.png";
 
 function App() {
-  const dispatch = useDispatch();
-  const blockchain = useSelector((state) => state.blockchain);
-  const data = useSelector((state) => state.data);
-  const [loading, setLoading] = useState (false);
-  const [status, setStatus] = useState ("");
-  const [NFTS, setNFTs] = useState ([]);
-  const ipfsBaseUrl = "https://nanimals.mypinata.cloud/ipfs/QmW7w6sgyYNfxLUKSodbAqEmoaD8LYwdRcw8tmPFh69uTF/";
-  // const numberOfTokens = 1;
-
-  function delay() {
-    setTimeout(function(){ setStatus("MINT A nEGG"); }, 2000);
-  }
-
-  const mint = (_uri) => {
-    setLoading(true);
-
-    blockchain.smartContract.methods
-      .mint(blockchain.account, _uri)
-      .send({from: blockchain.account})
-      .once("error", (err) => {
-        console.log(err);
-        setLoading(false);
-        setStatus("ERROR - PLEASE TRY AGAIN!");
-      })
-      .then((receipt) => {
-        console.log(receipt);
-        dispatch(fetchData(blockchain.account));
-        setLoading(false);
-        setStatus("nEgg SUCCESSFULLY MINTED!");
-        delay();
-      });
-  };
-
-  const fetchMetaDataForNFTS = useCallback(() => {
-    setNFTs([]);
-    data.userTokens.forEach((nft) => {
-      fetch(nft.uri)
-      .then((response) => response.json())
-      .then((metaData) => {
-        setNFTs((prevState) => [
-          ...prevState, 
-          {id: nft.id, metaData: metaData },
-        ]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    });
-  }, [data.userTokens]);
-
-  useEffect(() => {
-    if (blockchain.account !== "" && blockchain.smartContract !== null) {
-      dispatch(fetchData(blockchain.account));
-    }
-  }, [blockchain.account, blockchain.smartContract, dispatch]);
-
-  useEffect(() => {
-    fetchMetaDataForNFTS();
-  }, [fetchMetaDataForNFTS, data.userTokens]);
 
   return (
     <s.Screen>
