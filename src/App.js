723b867d83704678b05bc583d6f524bc3426bb09
seanbetts <seanbetts@icloud.com>
2021-10-14 09:55:03 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+import * as s from "./styles/globalStyles";
+import nEggLogo from "./images/nEggLogoTitle.png";
+import discord from "./images/discord.png";
+import twitter from "./images/twitter.png";
+
+function App() {
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [loading, setLoading] = useState (false);
+  const [status, setStatus] = useState ("");
+  const [NFTS, setNFTs] = useState ([]);
+  const ipfsBaseUrl = "https://nanimals.mypinata.cloud/ipfs/QmW7w6sgyYNfxLUKSodbAqEmoaD8LYwdRcw8tmPFh69uTF/";
+  // const numberOfTokens = 1;
+
+  function delay() {
+    setTimeout(function(){ setStatus("MINT A nEGG"); }, 2000);
+  }
+
+  const mint = (_uri) => {
+    setLoading(true);
+
+    blockchain.smartContract.methods
+      .mint(blockchain.account, _uri)
+      .send({from: blockchain.account})
+      .once("error", (err) => {
+        console.log(err);
+        setLoading(false);
+        setStatus("ERROR - PLEASE TRY AGAIN!");
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        dispatch(fetchData(blockchain.account));
+        setLoading(false);
+        setStatus("nEgg SUCCESSFULLY MINTED!");
+        delay();
+      });
+  };
+
+  const fetchMetaDataForNFTS = () => {
+    setNFTs([]);
+    data.userTokens.forEach((nft) => {
+      fetch(nft.uri)
+      .then((response) => response.json())
+      .then((metaData) => {
+        setNFTs((prevState) => [
+          ...prevState, 
+          {id: nft.id, metaData: metaData },
+        ]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    });
+  };
+
+  useEffect(() => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  }, [blockchain.smartContract, dispatch]);
+
+  useEffect(() => {
+    fetchMetaDataForNFTS();
+  }, [data.userTokens]);
+
+  return (
+    <s.Screen>
+      <s.Container>
+        <s.SpacerLarge />
+          <s.Row>
+            <s.Column>
+              <a href="https://discord.gg/CaC2wAxtyh" target="_blank" rel="noopener noreferrer">
+              <img src={discord} height={50} width={50} alt="discord"/>
+              </a>
+            </s.Column>
+            <s.SpacerSmall />
+            <s.Column>
+              <a href="https://www.twitter.com/nanimalsfun" target="_blank" rel="noopener noreferrer">
+              <img src={twitter} height={50} width={50} alt="twitter"/>
+              </a>
+            </s.Column>
+          </s.Row>
+          <img src={nEggLogo} height={600} width={600} alt="nEgg Logo"/>
+          <s.SpacerLarge />
+        </s.Container>
+        {blockchain.account === "" || blockchain.smartContract === null ? (
+        <s.Container>
+          <s.TextSubTitle>PLEASE CONNECT YOUR METAMASK WALLET</s.TextSubTitle>
+          <s.SpacerSmall />
+          <s.StyledButton
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(connect());
+            }}
+          >
+            CONNECT
+          </s.StyledButton>
+          <s.SpacerSmall />
+          {blockchain.errorMsg !== "" ? (
+            <s.TextDescription>{blockchain.errorMsg}</s.TextDescription>
+          ) : null}
+        </s.Container>
+      ) : (
+        <s.Container>
+          <s.TextSubTitle>nEggs ARE 1 MATIC EACH TO MINT</s.TextSubTitle>
+          <s.SpacerSmall />
+          <s.TextSubTitle>YOU CAN MINT UP TO 10 nEggs PER WALLET</s.TextSubTitle>
+          <s.SpacerSmall />
+          <s.StyledButton
+            onClick={(e) => {
+              e.preventDefault();
+              mint(ipfsBaseUrl);
+            }}
+          >          
+          {loading ? (
+            <>
+            <s.TextSubTitle2>minting...</s.TextSubTitle2>
+            </>
+          ) : (
+          status !== "" ? (
+            <>
+            <s.TextSubTitle2>{status}</s.TextSubTitle2>
+            </>
+          ) : <s.TextSubTitle2>MINT A nEGG</s.TextSubTitle2>)}
+          </s.StyledButton>
+          <s.SpacerLarge />
+          {NFTS.length>0 &&
+            <s.Container>
+              <s.TextTitle>YOUR nEggs COLLECTION</s.TextTitle>
+              <s.SpacerSmall />
+              <s.NFTContainerBar>
+                <s.NFTContainer>
+                  {data.loading ? (
+                  <>
+                    <s.TextDescription2>loading...</s.TextDescription2>
+                  </>
+                  ) : (
+                    NFTS.map((nft, index) => {
+                      return(
+                          <s.NFTSContainer key={index}>
+                            <img 
+                              alt={nft.metaData.name}
+                              src={nft.metaData.image.replace('ipfs://', 'https://nanimals.mypinata.cloud/ipfs/')} 
+                              width={200}
+                            />
+                            <s.SpacerSmall />
+                            <s.TextSubTitle>{nft.metaData.name}</s.TextSubTitle>
+                          </s.NFTSContainer>
+                      );
+                    })
+                  )}
+                </s.NFTContainer>
+              </s.NFTContainerBar>
+            </s.Container>
+          }
+        </s.Container>
+        )}
+        <s.Container>
+          <s.SpacerLarge />
+          <s.TextDescription>COPYRIGHT 2021 SEAN BETTS</s.TextDescription>
+          <s.SpacerSmall />
+          <s.TextDescription>CONTRACT: xxxxxxxxxxxxxxx</s.TextDescription>
+          <s.SpacerLarge />
+        </s.Container>
+    </s.Screen>
+  );
+}
+
+export default App;
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,158 @@
+import styled from "styled-components";
+
+// Used for wrapping a page component
+export const Screen = styled.div`
+  background-color: var(--blue);
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+// Used for providing space between components
+export const SpacerXSmall = styled.div`
+  height: 8px;
+  width: 8px;
+`;
+
+// Used for providing space between components
+export const SpacerSmall = styled.div`
+  height: 16px;
+  width: 16px;
+`;
+
+// Used for providing space between components
+export const SpacerMedium = styled.div`
+  height: 24px;
+  width: 24px;
+`;
+
+// Used for providing space between components
+export const SpacerLarge = styled.div`
+  height: 32px;
+  width: 32px;
+`;
+
+// Used for providing a wrapper around a component
+export const Container = styled.div`
+  display: flex;
+  flex: ${({ flex }) => (flex ? flex : 0)};
+  flex-direction: ${({ fd }) => (fd ? fd : "column")};
+  justify-content: ${({ jc }) => (jc ? jc : "flex-start")};
+  align-items: ${({ ai }) => (ai ? ai : "center")};
+  background-color: ${({ test }) => (test ? "pink" : "none")};
+  width: 100%;
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+`;
+
+export const TextTitle = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;
+  color: var(--pink);
+  font-size: 30px;
+  font-weight: 1000;
+`;
+
+export const TextSubTitle = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;  
+  color: var(--pink);
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+export const TextSubTitle2 = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;  
+  color: var(--black);
+  font-size: 20px;
+  font-weight: 1000;
+`;
+
+export const TextDescription = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif; 
+  color: var(--pink);
+  font-size: 14px;
+  font-weight: 400;
+`;
+
+export const TextDescription2 = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif; 
+  color: var(--yellow);
+  font-size: 20px;
+  font-weight: 1000;
+`;
+
+export const StyledClickable = styled.div`
+  :active {
+    opacity: 0.6;
+  }
+`;
+
+export const StyledButton = styled.button`
+  background-color: var(--pink);
+  color: black;
+  border: none;
+  width: 350px;
+  font-size: 20px;
+  font-weight: 1000;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px 0px;
+  cursor: pointer;
+
+  :hover {
+    background-color: var(--pink);
+    box-shadow: 0 9px var(--purple);
+  }
+
+  :active {
+    background-color: var(--pink);
+    box-shadow: 0 5px var(--purple);
+    transform: translateY(4px);
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const Column = styled.div`
+  flex: 50%;
+  padding: 10px;
+`;
+
+export const NFTContainerBar = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  background-color: var(--pink);
+`;
+
+export const NFTContainer = styled.div`
+  display: flex;
+  max-width: 1220px;
+  min-height: 306px;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  background-color: var(--pink);
+  padding: 10px;
+`;
+
+export const NFTSContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  width: 220px;
+  height: 266px;
+  align-items: ${({ ai }) => (ai ? ai : "center")};
+  background-color: var(--dirtywhite);
+  margin: 10px;
+  padding: 10px;
+`;
