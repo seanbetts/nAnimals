43fdfe58ef8e34eaf6953daf327800d1585bc72e
seanbetts <seanbetts@icloud.com>
2021-10-17 15:35:44 +0100

    format contract

--- contracts/nAnimals.sol
+++ contracts/nAnimals.sol
@@ -34,152 +34,238 @@ contract nAnimals is ERC721, ERC721Enumerable, ReentrancyGuard, Ownable, Pausabl
     constructor(string memory _initBaseURI) ERC721("nAnimals", "NANI") {
         setBaseURI(_initBaseURI);
     }
-    
-    // internal
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
-    }
-    
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
-        _tokenURIs[tokenId] = _tokenURI;
-    }
-    
-    function calculatePrice(uint256 numberOfTokens) internal view returns (uint256)
-    {
-      return mintPrice * numberOfTokens;
-    }
-
-    function concatJSON(string memory strOne, string memory strTwo) private pure returns (string memory) {
-        return string(abi.encodePacked(strOne, strTwo, ".json"));
-    }
-    
-    function concatStrings(string memory strOne, string memory strTwo, string memory strThree) private pure returns (string memory) {
-        return string(abi.encodePacked(strOne, strTwo, strThree));
-    }
-    
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
 
     // public
-    function mint(uint256 _mintQuantity) public payable whenNotPaused {
-        uint256 supply = totalSupply();
-        uint256 mintCost = calculatePrice(_mintQuantity);
-        string memory uri = baseURI;
-        require(_mintQuantity > 0, "Mint amount must be greater than 0");
-        require(_mintQuantity <= maxMintQuantity, concatStrings("You can currently only mint up to ", Strings.toString(maxMintQuantity), " nEGGs"));
-        if (msg.sender != owner()) {
-            require(balanceOf(msg.sender) + _mintQuantity <=10, "You can only mint up to 10 nEGGs per wallet");
-        }
-        require(supply + _mintQuantity <= maxMintSupply, "You're trying to mint more nEGGs than we have left!");
-        if (msg.sender != owner()) {
-            if(whitelisted[msg.sender] != true) {
-                require(msg.sender.balance >= mintCost, "Your wallet doesn't have enough MATIC for your transaction");
-                require(msg.value >= mintCost, "You haven't sent enough MATIC in the transaction to mint your nEGGs");
-                payable(msg.sender).transfer(mintCost);
+    function mint(uint256 _mintQuantity) 
+        public 
+        payable 
+        whenNotPaused 
+        {
+            uint256 supply = totalSupply();
+            uint256 mintCost = calculatePrice(_mintQuantity);
+            string memory uri = baseURI;
+            require(_mintQuantity > 0, "Mint amount must be greater than 0");
+            require(_mintQuantity <= maxMintQuantity, concatStrings("You can currently only mint up to ", Strings.toString(maxMintQuantity), " nEGGs"));
+            if (msg.sender != owner()) {
+                require(balanceOf(msg.sender) + _mintQuantity <=10, "You can only mint up to 10 nEGGs per wallet");
+            }
+            require(supply + _mintQuantity <= maxMintSupply, "You're trying to mint more nEGGs than we have left!");
+            if (msg.sender != owner()) {
+                if(whitelisted[msg.sender] != true) {
+                    require(msg.sender.balance >= mintCost, "Your wallet doesn't have enough MATIC for your transaction");
+                    require(msg.value >= mintCost, "You haven't sent enough MATIC in the transaction to mint your nEGGs");
+                    payable(msg.sender).transfer(mintCost);
+                }
+            }
+            for (uint256 i = 0; i < _mintQuantity; i++) {
+                uint256 newId = _tokenIds.current();
+                string memory newUri = concatJSON(uri, Strings.toString(newId));
+                _safeMint(msg.sender, newId);
+                _setTokenURI(newId, newUri);
+                _tokenIds.increment();
             }
         }
-        for (uint256 i = 0; i < _mintQuantity; i++) {
-            uint256 newId = _tokenIds.current();
-            string memory newUri = concatJSON(uri, Strings.toString(newId));
-            _safeMint(msg.sender, newId);
-            _setTokenURI(newId, newUri);
+    
+    function hatch(uint256 tokenId) 
+        public 
+        payable 
+        whenNotPaused 
+        nonReentrant 
+        {
+            string memory uri = concatJSON(hatchURI, Strings.toString(tokenId));
+            require(hatchingActive = true);
+            require(0 <= tokenId && tokenId <= 2000, "nEGG out of range");
+            require(balanceOf(_msgSender()) > 0, "nEGG needed to be able to hatch");
+            require(_msgSender() == ownerOf(tokenId), "You don't own this nEGG");
+            if (msg.sender != owner()) {
+                if(whitelisted[msg.sender] != true) {
+                    require(msg.sender.balance >= hatchPrice, "Your wallet doesn't have enough MATIC for your transaction");
+                    require(msg.value >= hatchPrice, "You haven't sent enough MATIC in the transaction to hatch your nEGG");
+                    payable(msg.sender).transfer(hatchPrice);
+                }
+            }
+            _safeMint(msg.sender, tokenId+genStart);
+            _setTokenURI(tokenId+genStart, uri);
             _tokenIds.increment();
         }
-    }
-    
-    function hatch(uint256 tokenId) public payable whenNotPaused nonReentrant {
-        string memory uri = concatJSON(hatchURI, Strings.toString(tokenId));
-        require(hatchingActive = true);
-        require(0 <= tokenId && tokenId <= 2000, "nEGG out of range");
-        require(balanceOf(_msgSender()) > 0, "nEGG needed to be able to hatch");
-        require(_msgSender() == ownerOf(tokenId), "You don't own this nEGG");
-        if (msg.sender != owner()) {
-            if(whitelisted[msg.sender] != true) {
-                require(msg.sender.balance >= hatchPrice, "Your wallet doesn't have enough MATIC for your transaction");
-                require(msg.value >= hatchPrice, "You haven't sent enough MATIC in the transaction to hatch your nEGG");
-                payable(msg.sender).transfer(hatchPrice);
+
+    function getUserTokens(address _owner) 
+        public 
+        view 
+        returns(RenderUserToken[] memory) 
+        {
+            uint256 ownerTokenCount = balanceOf(_owner);
+            RenderUserToken[] memory res = new RenderUserToken[](ownerTokenCount);
+            for(uint256 i = 0; i < ownerTokenCount; i++) {
+                uint256 tokenId = tokenOfOwnerByIndex(_owner, i);
+                res[i] = RenderUserToken(tokenId, tokenURI(tokenId));
             }
+            return res;
         }
-        _safeMint(msg.sender, tokenId+genStart);
-        _setTokenURI(tokenId+genStart, uri);
-        _tokenIds.increment();
-    }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns(string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        string memory _tokenURI = _tokenURIs[tokenId];
-        return _tokenURI;
-    }
-
-    function getUserTokens(address _owner) public view returns(RenderUserToken[] memory) {
-        uint256 ownerTokenCount = balanceOf(_owner);
-        RenderUserToken[] memory res = new RenderUserToken[](ownerTokenCount);
-        for(uint256 i = 0; i < ownerTokenCount; i++) {
-            uint256 tokenId = tokenOfOwnerByIndex(_owner, i);
-            res[i] = RenderUserToken(tokenId, tokenURI(tokenId));
+    function tokenURI(uint256 tokenId) 
+        public 
+        view 
+        virtual 
+        override 
+        returns(string memory) 
+        {
+            require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+            string memory _tokenURI = _tokenURIs[tokenId];
+            return _tokenURI;
         }
-        return res;
-    }
     
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
+    function supportsInterface(bytes4 interfaceId) 
+        public 
+        view 
+        virtual 
+        override(ERC721, ERC721Enumerable) 
+        returns (bool) 
+        {
+            return super.supportsInterface(interfaceId);
+        }
 
     // only owner
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        baseURI = _newBaseURI;
-    }
+    function setBaseURI(string memory _newBaseURI) 
+        public 
+        onlyOwner 
+        {
+            baseURI = _newBaseURI;
+        }
     
-    function setGenStart(uint256 _newGenStart) public onlyOwner {
-        genStart = _newGenStart;
-    }
+    function setGenStart(uint256 _newGenStart) 
+        public 
+        onlyOwner 
+        {
+            genStart = _newGenStart;
+        }
     
-    function setHatchURI(string memory _newHatchURI) public onlyOwner {
-        hatchURI = _newHatchURI;
-    }
+    function setHatchURI(string memory _newHatchURI) 
+        public 
+        onlyOwner 
+        {
+            hatchURI = _newHatchURI;
+        }
         
-    function pause() public onlyOwner {
-        _pause();
-    }
+    function pause() 
+        public 
+        onlyOwner 
+        {
+            _pause();
+        }
     
-    function unpause() public onlyOwner {
-        _unpause();
-    }
+    function unpause() 
+        public 
+        onlyOwner 
+        {
+            _unpause();
+        }
     
-    function hatchingOn() public onlyOwner {
-        require(genStart >= totalSupply() && genStart >= 2000, "genStart must be higher than the current supply");
-        require (keccak256(abi.encodePacked(hatchURI)) != keccak256(abi.encodePacked("")), "Remember to set the hatchURI");
-        hatchingActive = true;
-    }
+    function hatchingOn() 
+        public 
+        onlyOwner 
+        {
+            require(genStart >= totalSupply() && genStart >= 2000, "genStart must be higher than the current supply");
+            require (keccak256(abi.encodePacked(hatchURI)) != keccak256(abi.encodePacked("")), "Remember to set the hatchURI");
+            hatchingActive = true;
+        }
 
-    function hatchingOff() public onlyOwner {
-        hatchingActive = false;
-    }
+    function hatchingOff() 
+        public 
+        onlyOwner 
+            {
+            hatchingActive = false;
+        }
     
-    function setMintPrice(uint256 newMintPrice) public onlyOwner {
-        mintPrice = newMintPrice;
-    }
+    function setMintPrice(uint256 newMintPrice) 
+        public 
+        onlyOwner 
+        {
+            mintPrice = newMintPrice;
+        }
     
-    function setHatchPrice(uint256 newHatchPrice) public onlyOwner {
-        hatchPrice = newHatchPrice;
-    }
+    function setHatchPrice(uint256 newHatchPrice) 
+        public 
+        onlyOwner 
+        {
+            hatchPrice = newHatchPrice;
+        }
       
-    function setmaxMintAmount(uint256 _newmaxMintQuantity) public onlyOwner() {
-        maxMintQuantity = _newmaxMintQuantity;
-    }
+    function setmaxMintAmount(uint256 _newmaxMintQuantity) 
+        public 
+        onlyOwner() 
+        {
+            maxMintQuantity = _newmaxMintQuantity;
+        }
   
-    function whitelistUser(address _user) public onlyOwner {
-        whitelisted[_user] = true;
-    }
+    function whitelistUser(address _user) 
+        public 
+        onlyOwner 
+        {
+            whitelisted[_user] = true;
+        }
      
-    function removeWhitelistUser(address _user) public onlyOwner {
-        whitelisted[_user] = false;
-    }
+    function removeWhitelistUser(address _user) 
+        public 
+        onlyOwner 
+        {
+            whitelisted[_user] = false;
+        }
     
-    function withdrawMatic() public onlyOwner {
-        uint256 balance = address(this).balance;
-        payable(msg.sender).transfer(balance);
-    }
+    function withdrawMatic() 
+        public 
+        onlyOwner 
+        {
+            uint256 balance = address(this).balance;
+            payable(msg.sender).transfer(balance);
+        }
 
+    // internal
+    function _baseURI() 
+        internal 
+        view 
+        virtual 
+        override 
+        returns (string memory) 
+        {
+            return baseURI;
+        }
+    
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) 
+        internal 
+        {
+            _tokenURIs[tokenId] = _tokenURI;
+        }
+    
+    function calculatePrice(uint256 numberOfTokens) 
+        internal 
+        view 
+        returns (uint256)
+        {
+            return mintPrice * numberOfTokens;
+        }
+
+    function concatJSON(string memory strOne, string memory strTwo) 
+        private 
+        pure 
+        returns (string memory) 
+        {
+            return string(abi.encodePacked(strOne, strTwo, ".json"));
+        }
+    
+    function concatStrings(string memory strOne, string memory strTwo, string memory strThree) 
+        private 
+        pure 
+        returns (string memory) 
+        {
+            return string(abi.encodePacked(strOne, strTwo, strThree));
+        }
+    
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) 
+        internal 
+        virtual 
+        override(ERC721, ERC721Enumerable) 
+        {
+            super._beforeTokenTransfer(from, to, tokenId);
+        }
 }
