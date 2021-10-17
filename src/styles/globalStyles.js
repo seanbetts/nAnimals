684df0a4933fda62556f84c1064dd0a177d12aed
Sean Betts <seanbetts@icloud.com>
2021-10-17 21:11:36 +0100

    Updated styles

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -4,12 +4,13 @@ import styled from "styled-components";
 export const Screen = styled.div`
   background-color: var(--blue);
   background-image: ${({ image }) => (image ? `url(${image})` : "none")};
-  background-size: cover;
   background-position: center;
-  width: 100%;
-  min-height: 100vh;
+  min-width: 100%;
   display: flex;
   flex-direction: column;
+  overflow-x: hidden;
+  margin: 0;
+  padding: 0;
 `;
 
 // Used for providing space between components
@@ -50,25 +51,66 @@ export const Container = styled.div`
   background-position: center;
 `;
 
+export const ImageContainer = styled.div`
+  display: flex;
+  flex: ${({ flex }) => (flex ? flex : 0)};
+  flex-direction: ${({ fd }) => (fd ? fd : "column")};
+  justify-content: ${({ jc }) => (jc ? jc : "flex-start")};
+  align-items: ${({ ai }) => (ai ? ai : "center")};
+  background-color: ${({ test }) => (test ? "pink" : "none")};
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+`;
+
 export const TextTitle = styled.p`
   font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;
   color: var(--pink);
-  font-size: 30px;
+  font-size: 28px;
+  width: 90%;
+  max-width: 750px;
   font-weight: 1000;
+  text-align: center;
+`;
+
+export const TextTitle2 = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;
+  color: var(--black);
+  font-size: 28px;
+  width: 90%;
+  max-width: 750px;
+  font-weight: 1000;
+  text-align: center;
 `;
 
 export const TextSubTitle = styled.p`
   font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;  
   color: var(--pink);
-  font-size: 20px;
+  font-size: 18px;
+  width: 90%;
+  max-width: 750px;
   font-weight: 500;
+  text-align: center;
 `;
 
 export const TextSubTitle2 = styled.p`
   font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;  
   color: var(--black);
-  font-size: 20px;
+  font-size: 18px;
+  width: 90%;
+  max-width: 750px;
   font-weight: 1000;
+  text-align: center;
+`;
+
+export const TextSubTitle3 = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;  
+  color: var(--black);
+  font-size: 18px;
+  font-weight: 1000;
+  width: 90%;
+  max-width: 750px;
+  text-align: left;
 `;
 
 export const TextDescription = styled.p`
@@ -76,13 +118,28 @@ export const TextDescription = styled.p`
   color: var(--pink);
   font-size: 14px;
   font-weight: 400;
+  width: 90%;
+  max-width: 750px;
+  text-align: center;
+  line-height: 1.15;
 `;
 
 export const TextDescription2 = styled.p`
   font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif; 
   color: var(--yellow);
-  font-size: 20px;
+  font-size: 18px;
+  width: 90%;
+  max-width: 750px;
   font-weight: 1000;
+`;
+export const TextDescription3 = styled.p`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif; 
+  color: var(--black);
+  font-size: 14px;
+  font-weight: 500;
+  width: 90%;
+  max-width: 750px;
+  line-height: 1.15;
 `;
 
 export const StyledClickable = styled.div`
@@ -96,24 +153,44 @@ export const StyledButton = styled.button`
   color: black;
   border: none;
   width: 350px;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 1000;
+  text-align: center;
   padding: 10px;
   border-radius: 10px;
   margin: 10px 0px;
   cursor: pointer;
-
   :hover {
     background-color: var(--pink);
     box-shadow: 0 9px var(--purple);
   }
-
   :active {
     background-color: var(--pink);
     box-shadow: 0 5px var(--purple);
     transform: translateY(4px);
   }
 `;
+
+export const StyledButton2 = styled.button`
+  background-color: var(--pink);
+  color: black;
+  border: none;
+  width: 400px;
+  font-size: 18px;
+  font-weight: 1000;
+  text-align: center;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px 0px;
+`;
+
+export const ButtonName = styled.div`
+  font-family: Monaco, Tahoma, Arial, Helvetica, sans-serif;  
+  font-size: 18px;
+  font-weight: 1000;
+  text-align: center;
+`;
+
 
 export const Row = styled.div`
   display: flex;
@@ -129,7 +206,7 @@ export const NFTContainerBar = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
-  flex-direction: row;
+  flex-direction: column;
   background-color: var(--pink);
 `;
 
@@ -155,4 +232,8 @@ export const NFTSContainer = styled.div`
   background-color: var(--dirtywhite);
   margin: 10px;
   padding: 10px;
+`;
+
+export const Slider = styled.div`
+  width: 400px;
 `;
